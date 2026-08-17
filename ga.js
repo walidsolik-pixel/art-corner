30f5845228fed685609e5395513b01f114aa68b2
Walid Fathy <walid.solik@gmail.com>
2026-08-17 11:35:44 +0300

    Install Google Analytics 4 (Measurement ID: G-RPZ3MVBBSG)

---
+++ ga.js
@@ -0,0 +1,5 @@
+/* Google Analytics 4 (GA4) - Art Corner (Measurement ID: G-RPZ3MVBBSG) */
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-RPZ3MVBBSG');
